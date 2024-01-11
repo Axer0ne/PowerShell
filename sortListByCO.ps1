f9f562fbe793a78f4673d082d613f5a51538730d
Arman Habul <102185410+Axer0ne@users.noreply.github.com>
2024-01-11 16:15:40 +0100

    Update sortListByCO.ps1

--- sortListByCO.ps1
+++ sortListByCO.ps1
@@ -1,19 +1,8 @@
-﻿# Define the path to your input text file
 $inputFilePath = "Path"
-
-# Define the path to your output text file
 $outputFilePath = "Path"
-
-# Read the content of the input file
 $lines = Get-Content $inputFilePath
-
-# Filter lines that contain "CO-"
-$filteredLines = $lines | Where-Object { $_ -match "SOMEFILTER" }
-
-# Sort the filtered lines by name
+$filteredLines = $lines | Where-Object { $_ -match "CO-" }
 $sortedLines = $filteredLines | Sort-Object
 
-# Write the sorted lines to the output file
 $sortedLines | Out-File -FilePath $outputFilePath
-
 Write-Host "Filtered and sorted lines have been written to $outputFilePath"
