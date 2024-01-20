d0aeee76395e0879ae07f389628d2fb2e300619c
Arman Habul <102185410+Axer0ne@users.noreply.github.com>
2024-01-20 22:33:39 +0100

    Implemented full requirements
    - Admin account search
    - Fixed bug of finding multiple PCs

--- onPremDeactivation.ps1
+++ onPremDeactivation.ps1
@@ -1,13 +1,21 @@
 function FindUser($username) {
     try {
-        # Attempt to find user based on 'first.last@domain.com'
-        return Get-ADUser -Filter { UserPrincipalName -eq $username } -Properties UserPrincipalName, DisplayName, Enabled -ErrorAction Stop
+        $user = Get-ADUser -Filter { UserPrincipalName -eq $username } -Properties UserPrincipalName, DisplayName, Enabled -ErrorAction Stop
+
+        $adminUsername = "$"+$username
+        $adminUser = Get-ADUser -Filter { UserPrincipalName -eq $adminUsername } -Properties UserPrincipalName, DisplayName, Enabled -ErrorAction Stop
+
+        if ($null -ne $adminUser) {
+            Write-Host "This user has also an admin user: $($adminUser.UserPrincipalName)" -ForegroundColor Cyan
+            return @($user, $adminUser)
+        }
+
+        return @($user)
     } catch {
         Write-Host "Error finding user: $_" -ForegroundColor Red
         return $null
     }
 }
-
 function HideFromAddressList($user) {
     Write-Host "Hiding user from address list." -ForegroundColor Cyan
     try {
@@ -17,7 +25,6 @@ function HideFromAddressList($user) {
         Write-Host "Could not hide from the address list: $_" -ForegroundColor Red
     }
 }
-
 function RemoveManager($user) {
     Write-Host "Removing manager." -ForegroundColor Cyan
     try {
@@ -27,10 +34,20 @@ function RemoveManager($user) {
         Write-Host "Could not remove the manager: $_" -ForegroundColor Red
     }
 }
-
 function RemoveUserFromGroups($user) {
     try {
         $userGroups = Get-ADPrincipalGroupMembership $user | Select-Object Name
+        $displayName = (Get-ADUser -Identity $user -Properties DisplayName).DisplayName
+        Write-Host "Exporting user groups to a file." -ForegroundColor Cyan
+        $path = "C:\Terms\"
+        If(!(Test-Path -PathType container $path))
+        {
+            New-Item -ItemType Directory -Path $path
+        }
+        (Get-ADUser -Identity $user -Properties MemberOf | Select-Object MemberOf).MemberOf | Get-ADGroup | Select-Object name | Sort-Object -Property name | Out-File "C:\Terms\$displayName ($($user.SAMAccountName) - Group Membership.txt"
+        Write-Host "Group membership export complete. Stored on C:\Terms\" -ForegroundColor Green
+        Write-Host "Attach the user's C:\Terms\$displayName ($($user.SAMAccountName) - Group Membership.txt AD group membership to the RITM in ServiceNow." -ForegroundColor Green
+
         Write-Host "Removing user groups." -ForegroundColor Cyan
         foreach ($group in $userGroups) {
             try {
@@ -45,7 +62,6 @@ function RemoveUserFromGroups($user) {
         Write-Host "Error removing user from groups: $_" -ForegroundColor Red
     }
 }
-
 function DisableUserAccount($user) {
     Write-Host "Disabling user account." -ForegroundColor Cyan
     try {
@@ -55,7 +71,6 @@ function DisableUserAccount($user) {
         Write-Host "User could not be disabled: $_" -ForegroundColor Red
     }
 }
-
 function ChangeUserPassword($user) {
     Write-Host "Changing password." -ForegroundColor cyan
     try {
@@ -80,29 +95,35 @@ function ChangeUserPassword($user) {
         }
     }
 }
-
 function MoveUserToObsoleteOU($user) {
     Write-Host "Moving user to Obsolete OU." -ForegroundColor Cyan
     try {
-        $user | Move-ADObject -TargetPath "OU=Obsolete,DC=domain,DC=com" -ErrorAction Stop
+        $user | Move-ADObject -TargetPath "OU=Obsolete,DC=firmglobal,DC=com" -ErrorAction Stop
         Write-Host "SUCCESS: User moved to Obsolete OU." -ForegroundColor Green
     } catch {
         Write-Host "Error moving user to Obsolete OU: $_" -ForegroundColor Red
     }
 }
-
-function FindComputers($emailComponents) {
+function FindComputersByUPN($upnPrefix) {
     try {
-        $upnPrefix = $emailComponents.Split("@")[0]
         $computers = Get-ADComputer -Filter "Description -like '*$upnPrefix*'" -Properties Description, DisplayName, Enabled -ErrorAction Stop
         return $computers
     }
     catch {
-        Write-Host "Error finding computer: $_" -ForegroundColor Red
+        Write-Host "Error finding computers by UPN: $_" -ForegroundColor Red
         return $null
     }
 }
-
+function FindComputersBySAMAccountName($userSAMAccountName) {
+    try {
+        $computers = Get-ADComputer -Filter "Description -like '*$userSAMAccountName*'" -Properties Description, DisplayName, Enabled -ErrorAction Stop
+        return $computers
+    }
+    catch {
+        Write-Host "Error finding computers by SAMAccountName: $_" -ForegroundColor Red
+        return $null
+    }
+}
 function DisableComputer ($computer) {
     try {
         Write-Host "Disabling the computer object." -ForegroundColor Cyan
@@ -116,7 +137,6 @@ function DisableComputer ($computer) {
     }
     
 }
-
 function RemoveComputerGroups($computer) {
     try {
         $computerGroups = Get-ADPrincipalGroupMembership $computer | Select-Object Name
@@ -136,63 +156,87 @@ function RemoveComputerGroups($computer) {
     }
     
 }
-
 function MoveComputerToObsolete($computer) {
     Write-Host "Moving computer to Obsolete OU." -ForegroundColor Cyan
     try {
-        $computer | Move-ADObject -TargetPath "OU=Obsolete,OU=Workstations,DC=domain,DC=com"
+        $computer | Move-ADObject -TargetPath "OU=Obsolete,OU=Workstations,DC=firmglobal,DC=com"
         Write-Host "SUCCESS: Computer moved to Obsolete OU." -ForegroundColor Green
     } catch {
         Write-Host "Error moving computer to Obsolete OU: $_" -ForegroundColor Red
     }
 }
-
 #Script main
-Write-Host "Enter the username in 'first.last@domain.com' format:" -ForegroundColor Yellow
+Write-Host "Enter the username in 'first.last@forsta.com' format:" -ForegroundColor Yellow
 $username = Read-Host -Prompt "User"
 $emailComponents = $username.Split('@')
 Import-Module ActiveDirectory
+$userSAMAccountName = ""
 
-$user = FindUser($username)
-$foundComputers = FindComputers($emailComponents)
+$users = FindUser $username
 
-if ($user) {
-    Write-Host "User found:" -ForegroundColor Cyan
-    Write-Host "Username: $($user.SAMAccountName)" -ForegroundColor Green
-    Write-Host "Display Name: $($user.DisplayName)" -ForegroundColor Green    
-
-    $confirmation = ""
-    while ($confirmation -notmatch '^(Yes|No)$') {
-        Write-Host "Do you want to proceed with deactivation? (Type 'Yes' to proceed, 'No' to cancel):" -ForegroundColor Yellow
-        $confirmation = Read-Host -Prompt "Yes/No"
-        if ($confirmation -notmatch '^(Yes|No)$') {
-            Write-Host "Invalid input. Please type 'Yes' or 'No'." -ForegroundColor Red
+if($users) {
+    foreach ($user in $users) {
+        if ($user.UserPrincipalName -eq $username) {
+            $userSAMAccountName = $users.SAMAccountName
         }
     }
+}
 
-    if ($confirmation -eq 'Yes') {
-        Write-Host ""
-        HideFromAddressList($user)
-        Write-Host ""
-        RemoveManager($user)
-        Write-Host ""
-        RemoveUserFromGroups($user)
-        Write-Host ""
-        DisableUserAccount($user)
-        Write-Host ""
+$foundComputersByUPN = FindComputersByUPN $emailComponents[0]
+$foundComputersBySAM = FindComputersBySAMAccountName $userSAMAccountName
+
+#Combine the results
+$foundComputers = @()
+if ($foundComputersByUPN) {
+    $foundComputers += $foundComputersByUPN
+}
+if ($foundComputersBySAM.SAMAccountName) {
+    $foundComputers += $foundComputersBySAM
+}
+
+$foundComputers = $foundComputers | Select-Object -Unique
+
+if ($users) {
+    
+    foreach($user in $users) {
+        Write-Host "User found:" -ForegroundColor Cyan
+        Write-Host "Username: $($user.SAMAccountName)" -ForegroundColor Green
+        Write-Host "Display Name: $($user.DisplayName)" -ForegroundColor Green    
+
+        $confirmation = ""
+        while ($confirmation -notmatch '^(Yes|No)$') {
+            Write-Host "Do you want to proceed with deactivation? (Type 'Yes' to proceed, 'No' to cancel):" -ForegroundColor Yellow
+            $confirmation = Read-Host -Prompt "Yes/No"
+            if ($confirmation -notmatch '^(Yes|No)$') {
+                Write-Host "Invalid input. Please type 'Yes' or 'No'." -ForegroundColor Red
+            }
+        }
+
+        if ($confirmation -eq 'Yes') {
+            Write-Host ""
+            HideFromAddressList($user)
+            Write-Host ""
+            RemoveManager($user)
+            Write-Host ""
+            RemoveUserFromGroups($user)
+            Write-Host ""
+            DisableUserAccount($user)
+            Write-Host ""
         
-        $passwordChangeResult = ChangeUserPassword($user)
-        if($passwordChangeResult["changed"]) {
-            Write-Host "Password for $($user.Name) has been set to: $($passwordChangeResult["password"]) and must be changed on next logon." -ForegroundColor Green
+            $passwordChangeResult = ChangeUserPassword($user)
+            if($passwordChangeResult["changed"]) {
+                Write-Host "Password for $($user.Name) has been set to: $($passwordChangeResult["password"]) and must be changed on next logon." -ForegroundColor Green
+            } else {
+                Write-Host "Password for $($user.Name) could not be changed." -ForegroundColor Red
+            }
+        
+            Write-Host ""
+            MoveUserToObsoleteOU($user)
         } else {
-            Write-Host "Password for $($user.Name) could not be changed." -ForegroundColor Red
+            Write-Host "Deactivation process terminated by the admin." -ForegroundColor Red
         }
-        
-        Write-Host ""
-        MoveUserToObsoleteOU($user)
-    } else {
-        Write-Host "Deactivation process terminated by the admin." -ForegroundColor Red
     }
+    
 } else {
     Write-Host "User not found in the specified formats." -ForegroundColor Red
 }
@@ -200,7 +244,7 @@ if ($user) {
 Write-Host ""
 Write-Host ""
 
-if ($foundComputers -and $user) {
+if ($foundComputers -and $users) {
     
     Write-Host "~Computer Deactivation~"
     
